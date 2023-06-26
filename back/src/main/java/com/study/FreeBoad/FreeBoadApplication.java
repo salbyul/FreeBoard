d994e23f4bd8a23f706edd7fa04c943171ab719e
salbyul <kpast96@gmail.com>
2023-06-27 01:02:29 +0900

    Initial commit

---
+++ back/src/main/java/com/study/FreeBoad/FreeBoadApplication.java
@@ -0,0 +1,13 @@
+package com.study.FreeBoad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeBoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeBoadApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/study/FreeBoad/FreeBoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.FreeBoad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeBoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
